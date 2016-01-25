9cac7bb6b9825dd7baeb812cb30fcbc01ce3ec61
Andrew Cho and Tim Durward <timothy.a.durward@gmail.com>
2016-01-24 18:19:46 -0800

    add prototype

--- js/master.js
+++ js/master.js
@@ -3,3 +3,10 @@ function Contact(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
 }
+
+Contact.prototype.fullName = function() {
+  return this.firstName + this.lastName;
+}
+
+
+var andyContact = new Contact("Andrew ", "Cho");
